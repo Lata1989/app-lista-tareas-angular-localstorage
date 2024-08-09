a1786ba9b3fe370a7ff2a0852c984a8858809365
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2024-08-08 23:49:17 -0300

    lista de tareas en angular 18 con localstorage terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'app-lista-tareas-angular-localstorage';
+
+export class AppComponent implements OnInit{
+
+  listaTareas: string[] = [];
+  nuevaTarea:string = '';
+  indice: number = 0;
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+  // title = 'app-lista-tareas-angular-localstorage';
+
+  private _tareasService = inject(TareasService);
+
+  eliminarTarea(index: number) {
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  };
+
+  agregarTarea() {
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.indice = this.indice + 1;
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
 }
